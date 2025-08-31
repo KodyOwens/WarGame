a178e9ebaef2c69a8f6aa60c54f10bc2aa1a6805
kenobinow <kenobinow@gmail.com>
2025-08-31 23:51:47 +0000

    feat: Add stateless profile section

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,66 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'User Profile Card';
+    return MaterialApp(
+      title: appTitle,
+      home: const ProfileScreen(),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Profile'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          ProfileCard(),
+          SizedBox(height: 30),
+          FollowCounter(),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        CircleAvatar(
+          radius: 50,
+          backgroundImage: NetworkImage(
+            'https://images.unsplash.com/photo-1528821154947-1aa3d1b74941?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+          ),
+          SizedBox(height: 16),
+        Text(
+          'Ramon Venezuela',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          'Software Developer',
+          style: TextStyle(fontSize: 18, color: Colors.grey),
+        ),
+        Text(
+          'RamonVenezuela@gmail.com',
+          style: TextStyle(fontSize: 16, color: Colors.blueGrey),
+        ),
+      ],
+    );
+  }
+}
+
