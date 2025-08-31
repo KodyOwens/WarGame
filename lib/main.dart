86f91308de1c08cbaa87295d049560010368e4e8
kenobinow <kenobinow@gmail.com>
2025-08-31 23:43:04 +0000

    feat: Initial commit with Flutter boilerplate

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+// My Student ID # is 90032598 
+// The date I submitted this is: 8/31/2025
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
